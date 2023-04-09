d5b71af7e4c4ace0dce9e6a73b6981bd7f56284e
Travis Logan <c1014427@newcastle.ac.uk>
2023-04-09 12:07:05 +0100

    added sticky headers to allow for the webpage to be constant at different aspect ratios.

--- js/script.js
+++ js/script.js
@@ -16,4 +16,10 @@ window.onscroll = () => {
             })
         }
     })
+
+    /* ----- sticky navbar ----- */
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
 }
